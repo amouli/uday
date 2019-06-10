1bb50351a7169ab4e208a87643719e273a61ef22
amouli <mouliambati1994@gmail.com>
2019-06-10 19:55:23 +0530

    first commit

---
+++ src/main/java/error/hello.java
@@ -0,0 +1,19 @@
+package error;
+
+public class hello{
+
+   public String result;
+     
+     public hello() {
+       // TODO Auto-generated constructor stub
+     }
+     public String ConcatAB(String value1, String value2) {
+       result = value1 + value2; 
+       return result;
+     }
+     public static int staticFunctionTest(int a,int b) {
+   	  int c = a+b;
+return c;
+   	
+     }
+}
